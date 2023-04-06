c404a6a52f5db9932ac7b3ce571c0d450cb4a820
duyhehehe <duy20036athcsns@gmail.com>
2023-04-06 22:50:55 +0700

    create scss

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
-const app = express()
+const {engine} = require('express-handlebars');
+
+const app = express();
 const port = 8080;
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(morgan('combined'));
 
+app.engine('hbs', engine(
+    {
+        extname: '.hbs'
+    }
+));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'rsc\\views'));
+
 app.get('/home', (req, res) => {
-    res.send(
-        `<h1 style="color: red">Hello World!</h1>`
-    )
-});
+    res.render('home');
+})
+
+app.get('/news', (req, res) => {
+    res.render('news');
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)

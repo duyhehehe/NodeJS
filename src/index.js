d512f928b63764f7fd347c99ebe5b1dd3606d320
duyhehehe <duy20036athcsns@gmail.com>
2023-04-12 17:24:06 +0700

    create routes

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const {engine} = require('express-handlebars');
+const route=require('./routes')
 
 const app = express();
 const port = 8080;
@@ -24,22 +25,8 @@ app.engine('hbs', engine(
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'rsc\\views'));
 
-app.get('/home', (req, res) => {
-    res.render('home');
-});
-
-app.get('/news', (req, res) => {
-    res.render('news');
-});
-
-app.post('/search', (req, res) => {
-    console.log(req.body);
-    res.send('');
-});
-
-app.get('/search', (req, res) => {
-    res.render('search');
-});
+// Routes init
+route(app);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)

b1fcd2e725d5ea9b2413b6371f7c6c0e5584abaa
duyhehehe <duy20036athcsns@gmail.com>
2023-04-12 16:04:12 +0700

    use urlencoded

--- src/index.js
+++ src/index.js
@@ -10,6 +10,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(morgan('combined'));
 
+app.use(express.urlencoded({
+    extended: true
+}));
+
+app.use(express.json());
+
 app.engine('hbs', engine(
     {
         extname: '.hbs'
@@ -20,11 +26,20 @@ app.set('views', path.join(__dirname, 'rsc\\views'));
 
 app.get('/home', (req, res) => {
     res.render('home');
-})
+});
 
 app.get('/news', (req, res) => {
     res.render('news');
-})
+});
+
+app.post('/search', (req, res) => {
+    console.log(req.body);
+    res.send('');
+});
+
+app.get('/search', (req, res) => {
+    res.render('search');
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)

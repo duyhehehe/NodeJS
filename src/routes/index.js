65be577d85ded4ebb77e8617b87cd44db3fa242c
duyhehehe <duy20036athcsns@gmail.com>
2023-04-13 23:32:40 +0700

    test prettier, lint-staged

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,6 @@ const newsRouter = require('./news');
 const siteRouter = require('./site')
 
 function route(app) {
-    app.use('/search', siteRouter);
-
     app.use('/news', newsRouter);
 
     app.use('/', siteRouter);
